7b9ad8aa386969eccd7d9d55f97eb1f3b0cb8b92
p-baleine <p.baleine@gmail.com>
2020-05-14 20:08:40 +0900

    Add Example model.
    references #7.

--- editor/models.py
+++ editor/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
-# Create your models here.
+
+# class User(models.Model):
+#     pass
+
+
+class Example(models.Model):
+    id = models.CharField(primary_key=True, max_length=128)
+    url = models.CharField(max_length=256)
+    hash = models.CharField(max_length=64)
+    title = models.CharField(max_length=256)
+    content = models.TextField()
+    fetched_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.id
+
+
+# class AnnotatedExample(models.Model):
+#     pass
