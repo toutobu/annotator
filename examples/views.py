ed2dbaa28872224f5e859f570915b11fa4df1b03
p-baleine <p.baleine@gmail.com>
2020-05-16 07:15:32 +0900

    Auth0 のスコープを使うようにする
    related #13.
    
    色々やったけどあまり覚えていない…たしかここら辺を修正したと思う
    
    * server の examples.permission を利用するようにした
    * fronend から scope の情報を付与するようにした
    * google-oauth2 の dev じゃない版を新規作成した
    * /etc/hosts に toutobu-annotator.local を追加
        * consent 周りで localhost じゃうまくいかないため
    * dev-server で localhost じゃなくなったことへの対応
    * auth0-spa-js で怒るので、dev-server で https を設定
    
    つうか、Auth0 まともに使うの無茶苦茶大変じゃないか？？？？

--- examples/views.py
+++ examples/views.py
@@ -12,5 +12,4 @@ logger = logging.getLogger(__name__)
 class IndexViewSet(viewsets.ModelViewSet):
     queryset = Example.objects.all()
     serializer_class = ExampleSerializer
-    # FIXME: Scope の設定がうまくいかない(…今回は必要ないけど)
-    # permission_classes = [permissions.ReadExamplesScope]
+    permission_classes = [permissions.ReadExamplesScope]
