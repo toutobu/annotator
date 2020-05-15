a2ca1a8868d086fda98543217077374619f19650
p-baleine <p.baleine@gmail.com>
2020-05-15 11:49:37 +0900

    Basic login.
    related #4.

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -3,6 +3,27 @@ import App from './App.vue';
 import router from './router';
 import store from './store';
 
+// Import the Auth0 configuration
+import { domain, clientId } from '../auth_config.json';
+
+// Import the plugin here
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const { Auth0Plugin } = require('./auth');
+
+// Install the authentication plugin here
+Vue.use(Auth0Plugin, {
+  domain,
+  clientId,
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  onRedirectCallback: (appState: any) => {
+    router.push(
+      appState && appState.targetUrl
+        ? appState.targetUrl
+        : window.location.pathname,
+    );
+  },
+});
+
 Vue.config.productionTip = false;
 
 new Vue({
